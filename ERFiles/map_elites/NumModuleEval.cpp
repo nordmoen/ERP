751eb8b8c27d63767a6bde8ec7eb42ff47110cc3
Jørgen Nordmoen <jorgen.nordmoen@gmail.com>
2019-10-15 08:54:30 +0200

    Changed to counting movable and total modules

--- ERFiles/map_elites/NumModuleEval.cpp
+++ ERFiles/map_elites/NumModuleEval.cpp
@@ -6,20 +6,18 @@ namespace map_elites {
 const std::pair<float, float> NumModuleEval::behavior(const std::shared_ptr<Genome> genome) const {
   const auto modules = genome->morph->createdModules;
   float movable = 0.0;
-  float non_movable = 0.0;
   for(const auto module : modules) {
     // '4' means that the module should be movable
     if(module->type == 4) {
       movable += 1;
-    } else {
-      non_movable += 1;
     }
   }
   float max = static_cast<float>(Settings::getInstance()->maxAmountModules);
+  float total = static_cast<float>(modules.size());
   movable /= max;
-  non_movable /= max;
+  total /= max;
   assert(0.0 <= movable && movable <= 1.0);
-  assert(0.0 <= non_movable && non_movable <= 1.0);
-  return std::make_pair(non_movable, movable);
+  assert(0.0 <= total && total <= 1.0);
+  return std::make_pair(total, movable);
 }
 }
